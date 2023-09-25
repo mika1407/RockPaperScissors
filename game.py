542a50591569a80af5d85770b4a2b8631f4da51d
mika tiihonen <koneautomaatio@hotmail.com>
2023-09-25 12:34:49 +0300

    while loop

--- game.py
+++ game.py
@@ -3,5 +3,10 @@ import random
 choices = ["rock","paper","scissors"]
 
 computer = random.choice(choices)
+player = None
 
-print(computer)
+while player not in choices:
+  player = input("rock, paper or scissors?: ")
+
+print("computer: ", computer)
+print("player: ", player)
